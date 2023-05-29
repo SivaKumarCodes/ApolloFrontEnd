167749cff470190accfc9b2e7112acea8f69498b
siva kumar <sivakumar.av23@gmail.com>
2023-05-30 02:46:29 +0530

    Created Sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { dropdownOption } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,4 +9,48 @@ import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 })
 export class SidebarComponent {
   rightIcon = faChevronRight;
+
+  navbarOptions: string[] = [
+    'Vitamins & Nutriton',
+    'Healthcare Devices',
+    'Personal Care',
+    'Ayurveda Products',
+    'Homeopathy',
+  ];
+
+  options: dropdownOption[][] = [
+    [
+      {
+        url: '/assets/multivitamin.png',
+        name: 'Multivitamins',
+        link: 'Multivitamin',
+      },
+      {
+        url: '/assets/nutritionalDrinks.webp',
+        name: 'Nutritional Drinks',
+        link: 'Nutritional Drinks',
+      },
+      {
+        url: '/assets/proteinSupplements.webp',
+        name: 'Protein Supplements',
+        link: 'Protein Supplements',
+      },
+      {
+        url: '/assets/fishOil.webp',
+        name: 'Omega3 & Fish oil supplements',
+        link: 'Omega3',
+      },
+    ],
+    [
+      {
+        url: '/assets/bloodGlucose.webp',
+        name: 'Blood Glucose Monitors',
+        link: 'Blood Glucose Monitors',
+      },
+    ],
+    [],
+    [],
+    [],
+    [],
+  ];
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
+    colors: {
+      ...colors,
+      main: "#3a8885",
+      lightgrey: "#999999",
+      thinborder: "#e0e0e0",
+      // lightgrey: "#5F7161",
+    },
   },
   plugins: [],
 };
