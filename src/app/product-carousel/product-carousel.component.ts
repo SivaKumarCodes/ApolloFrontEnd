c8b21fa345613606924fd6156096984472b38ea9
siva kumar <sivakumar.av23@gmail.com>
2023-11-08 19:41:45 +0530

    product carousel completed.

---
+++ src/app/product-carousel/product-carousel.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { activeProduct } from '../store/app.selectors';
+import { Product } from '../store/app.store';
+
+@Component({
+  selector: 'app-product-carousel',
+  templateUrl: './product-carousel.component.html',
+  styleUrls: ['./product-carousel.component.css'],
+})
+export class ProductCarouselComponent {
+  product!: Product;
+  imgsLength: number = 0;
+  imgs: string[] = [];
+  activeImage: number = 0;
+  margin: number = 0;
+  slideWidth: number = 0;
+
+  constructor(private store: Store) {}
+
+  swipeLeft() {
+    console.log('left');
+    if (this.activeImage < this.imgsLength - 1) {
+      ++this.activeImage;
+      this.margin = this.activeImage * 100;
+    } else if (this.activeImage == this.imgsLength - 1) {
+      this.activeImage = 0;
+      this.margin = 0;
+    }
+  }
+
+  swipeRight() {
+    console.log('right');
+    if (this.activeImage == 0) {
+      this.activeImage = this.imgsLength - 1;
+      this.margin = (this.imgsLength - 1) * 100;
+    } else {
+      --this.activeImage;
+      this.margin = this.activeImage * 100;
+    }
+  }
+
+  ngOnInit(): void {
+    this.store.select(activeProduct).subscribe((p) => {
+      this.product = p!;
+      this.imgsLength = p?.variants[0].images.length!;
+      this.imgs = p?.variants[0].images!;
+    });
+
+    this.slideWidth = (this.imgsLength * 100) / this.imgsLength;
+  }
+}
--- src/app/product-page/product-page.module.ts
+++ src/app/product-page/product-page.module.ts
@@ -8,11 +8,13 @@ import { AppModule } from '../app.module';
 import { LoadingComponent } from '../loading/loading.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { LandingPageModule } from '../landing-page/landing-page.module';
+import { ProductCarouselComponent } from '../product-carousel/product-carousel.component';
 
 @NgModule({
   declarations: [
     ProductPageComponent,
     ProductViewerComponent,
+    ProductCarouselComponent,
     LoadingComponent,
   ],
   imports: [
